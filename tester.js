0b38526e3b0089ec7555170497b8665cce47dae3
Anna Yesayan <anna_yesayan@edu.aua.am>
2022-11-16 21:29:10 +0400

    Updates second commit!

--- tester.js
+++ tester.js
@@ -1,5 +1,5 @@
 const ex2 = require('./exercise2');
 const factorialValue = ex2.factorialPrint(5);
-const gcdValue = ex2.gcdPrint(28, 12);
-const lcmValue = ex2.lcmPrint(28, 12);
+const gcdValue = ex2.gcdPrint(12, 8);
+const lcmValue = ex2.lcmPrint(12, 8);
 const hexaValue = ex2.hexaPrint(1234);
